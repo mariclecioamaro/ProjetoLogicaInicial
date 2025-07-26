272379fe886a2cde30324e55a3e20619cda0536d
mariclecioamaro@gmail.com <mariclecioamaro@gmail.com>
2025-07-25 21:11:57 -0300

    Correção de bug

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-do{
+let op;
+
+do {
     // Solicita que o usuário digite o nome do herói
-    let nome = console.log("Digite o nome do herói:");
+    let nome = prompt("Digite o nome do herói:");
 
     // Solicita que o usuário digite a quantidade de XP
-    let xp = parseInt(console.log("Digite a quantidade de XP do herói:"));
+    let xp = parseInt(prompt("Digite a quantidade de XP do herói:"));
 
     // Variável que armazena o nível
     let nivel = "";
 
     // Estrutura de decisão que determina o nível
     if (xp < 1000) {
-    nivel = "Ferro";
+        nivel = "Ferro";
     } else if (xp >= 1001 && xp <= 2000) {
-    nivel = "Bronze";
+        nivel = "Bronze";
     } else if (xp >= 2001 && xp <= 5000) {
-    nivel = "Prata";
+        nivel = "Prata";
     } else if (xp >= 5001 && xp <= 7000) {
-    nivel = "Ouro";
+        nivel = "Ouro";
     } else if (xp >= 7001 && xp <= 8000) {
-    nivel = "Platina";
+        nivel = "Platina";
     } else if (xp >= 8001 && xp <= 9000) {
-    nivel = "Ascendente";
+        nivel = "Ascendente";
     } else if (xp >= 9001 && xp <= 10000) {
-    nivel = "Imortal";
+        nivel = "Imortal";
     } else if (xp >= 10001) {
-    nivel = "Radiante";
+        nivel = "Radiante";
     } else {
-    nivel = "Indefinido";
+        nivel = "Indefinido";
     }
 
     // Exibe mensagem com o resultado
-    console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
-}while (op !== 0);
+    alert(`O Herói de nome ${nome} está no nível de ${nivel}`);
 
-let op = parseInt(console.log("Digite 0 para sair do programa ou qualquer tecla para continuar"));
+    // Pergunta se o usuário deseja continuar
+    op = prompt("Digite 0 para sair do programa ou qualquer outra tecla para continuar:");
 
+} while (op !== "0");
