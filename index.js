f794b39a45e65bba01ee61a0ac34e20c104e2128
mariclecioamaro@gmail.com <mariclecioamaro@gmail.com>
2025-07-25 21:35:35 -0300

    Alteração do código

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
-let op;
+// Definindo o nome e a XP do herói
+let nome= console.log("Digite o nome do herói: ");
+let xp = console.log(parseInt("Digite os pontos de xp: ")); // Altere esse valor para testar diferentes níveis
 
-do {
-    // Solicita que o usuário digite o nome do herói
-    let nome = prompt("Digite o nome do herói:");
+// Variável para armazenar o nível
+let nivel = "";
 
-    // Solicita que o usuário digite a quantidade de XP
-    let xp = parseInt(prompt("Digite a quantidade de XP do herói:"));
+// Estrutura de decisão para classificar o nível do herói
+if (xp < 1000) {
+  nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+  nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+  nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+  nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+  nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+  nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+  nivel = "Imortal";
+} else if (xp >= 10001) {
+  nivel = "Radiante";
+}
 
-    // Variável que armazena o nível
-    let nivel = "";
-
-    // Estrutura de decisão que determina o nível
-    if (xp < 1000) {
-        nivel = "Ferro";
-    } else if (xp >= 1001 && xp <= 2000) {
-        nivel = "Bronze";
-    } else if (xp >= 2001 && xp <= 5000) {
-        nivel = "Prata";
-    } else if (xp >= 5001 && xp <= 7000) {
-        nivel = "Ouro";
-    } else if (xp >= 7001 && xp <= 8000) {
-        nivel = "Platina";
-    } else if (xp >= 8001 && xp <= 9000) {
-        nivel = "Ascendente";
-    } else if (xp >= 9001 && xp <= 10000) {
-        nivel = "Imortal";
-    } else if (xp >= 10001) {
-        nivel = "Radiante";
-    } else {
-        nivel = "Indefinido";
-    }
-
-    // Exibe mensagem com o resultado
-    alert(`O Herói de nome ${nome} está no nível de ${nivel}`);
-
-    // Pergunta se o usuário deseja continuar
-    op = prompt("Digite 0 para sair do programa ou qualquer outra tecla para continuar:");
-
-} while (op !== "0");
+// Exibindo a mensagem final
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
